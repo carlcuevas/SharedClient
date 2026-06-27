ea966f9bc96ee22237d83363127f5ce23520d8c2
carlcuevas <carl.cuevasn@duocuc.cl>
2026-06-26 23:58:39 -0400

    test: provocar fallo intencional en tests

--- tests/ejemplo.test.js
+++ tests/ejemplo.test.js
@@ -1,5 +1,5 @@
 const { salud } = require('../index');
 
 test('health retorna status ok', () => {
-  expect(salud()).toEqual({ status: 'ok' });
+  expect(salud()).toEqual({ status: 'FALLO_INTENCIONAL' });
 });
